cc504523501a814596df31681bad963d28e42120
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2025-09-09 00:01:04 +0500

    Update Resizing.tsx

--- Resizing.tsx
+++ Resizing.tsx
@@ -1,1 +1,10 @@
-// Handling dynamic Input Field Height w.r.t content:
+// Handling dynamic Input Field Height w.r.t content:  
+<div>
+ <textarea 
+   placeholder={"ask me anything!"}
+   value={value}
+   onChange={(e) => handleChange(e.target.value)}
+   />
+   
+
+</div>
