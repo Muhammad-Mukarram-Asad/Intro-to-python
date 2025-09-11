621fe008df18d269e23e822928d19aaf727fd9c7
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2025-09-11 23:58:34 +0500

    Update Resizing.tsx

--- Resizing.tsx
+++ Resizing.tsx
@@ -1,8 +1,13 @@
 // Handling dynamic Input Field Height w.r.t content:  
+const handleInput = () => {
+}
+
+
 <div>
  <textarea 
    placeholder={"ask me anything!"}
    value={value}
+   onInput={handleInput}
    onChange={(e) => handleChange(e.target.value)}
    />
    
