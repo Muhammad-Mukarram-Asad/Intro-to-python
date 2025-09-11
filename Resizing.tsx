c51795efaee92f0f0b606c20abd2541ed66c4421
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2025-09-12 00:01:59 +0500

    Update Resizing.tsx

--- Resizing.tsx
+++ Resizing.tsx
@@ -1,10 +1,13 @@
 // Handling dynamic Input Field Height w.r.t content:  
+import {useRef} from 'react';
 const handleInput = () => {
+ const mainRef = useRef(mainDivRef);
 }
 
 
-<div>
+<div ref={mainDivRef}>
  <textarea 
+  ref={textareaRef}
    placeholder={"ask me anything!"}
    value={value}
    onInput={handleInput}
